caac51620ddd24d17417d183184bc86d420823d7
KevinLindemark <reclaimyourdata@tutamail.com>
2024-10-30 11:01:18 +0100

    tilføjet kommentarer

--- oevelse2/oevelse_2_2.py
+++ oevelse2/oevelse_2_2.py
@@ -1,6 +1,7 @@
 from machine import Pin, deepsleep
 from random import choice
 from neopixel import NeoPixel
+from time import sleep
 """
 2.2 - Prøv at vække ESP32 fra deepsleep med en timer,
 og kør en funktion der sætter en random farve på
@@ -17,5 +18,7 @@ np[0] = (choice(colors),choice(colors),choice(colors))
 # set second pixel where R, G, B each gets random value of 255 or 0
 np[1] = (choice(colors),choice(colors),choice(colors))
 np.write()
+# sleep is inserted to more easely stop the program between deepsleep
+sleep(1) 
 # goint to deepsleep for 1000 miliseconds
 deepsleep(1000)

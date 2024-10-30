c22ba9b9aa5ac4b63a2b5df33f7ad6dd8aa2f541
KevinLindemark <reclaimyourdata@tutamail.com>
2024-10-30 09:33:57 +0100

    øvelse2 tilføjet med styring af led1 lysintensitet ud fra ultralydsmodul afstand i CM

--- oevelse1/oevelse1_1.py
+++ oevelse1/oevelse1_1.py
@@ -1,7 +1,18 @@
 from hcsr04 import HCSR04
 from machine import Pin
 from time import ticks_ms, sleep_ms
+"""
+1.1 - upload modulet til at arbejde med ultralydsmodulet til ESP32 og læg den I mappen lib. ​
+https://randomnerdtutorials.com/micropython-hc-sr04-ultrasonic-esp32-esp8266/ ​
 
+Lav et uendeligt loop med et non-blocking delay der printer distancen i CM
+der måles af HC-SR04 ultralydssensoren hvert 100 millisekund.
+(Se eksemplerne fra artiklen,og e bort fra afsnittet med Oled displayet).
+Brug trigger_pin=15, echo_pin=34, og bemærk at
+GPIO 34 deler kredsløb med potmeter på educaboard og
+potmeter skal ikke være drejet helt til højre eller venstre ellers får man ugyldige målinger!)​
+
+"""
 usonic = HCSR04(trigger_pin=15, echo_pin=34, echo_timeout_us=10000)
 
 start = ticks_ms()
